ca1a6d7e602a1d8394a5e119947525cab8d77822
Oscar 8A <oscarochoacerrato@gmail.com>
2022-06-22 17:38:38 -0400

    print truth tables for && and ||

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        System.out.println("Truth table for && operator");
+        System.out.println("true && true = " + (true && true));
+        System.out.println("true && false = " + (true && false));
+        System.out.println("false && false = " + (false && false));
+        System.out.println("false && true = " + (false && true));
+        System.out.println("---------------------------------");
+        System.out.println("Truth table for || operator");
+        System.out.println("true || true = " + (true || true));
+        System.out.println("true || false = " + (true || false));
+        System.out.println("false || false = " + (false || false));
+        System.out.println("false || true = " + (false || true));
     }
 }
